09385209bb9672e7f075228e0693266d7abbced2
Anish Man Shrestha <anish.shrestha237@gmail.com>
2016-06-05 14:35:23 -0500

    Project Added

---
+++ ChatApplication/ChatApplication/Hubs/ChatHub.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Signar2.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private static int clientCounter = 0;
+
+        
+        public void Send(string name, string message)
+        {
+            Clients.All.addNewMessageToPage(name, message);
+        }
+
+        public override System.Threading.Tasks.Task OnConnected()
+        {
+            clientCounter++;
+            Clients.All.recalculateOnlineUsers(clientCounter);
+            return base.OnConnected();
+        }
+
+        public override System.Threading.Tasks.Task OnDisconnected(bool stopCalled)
+        {
+            clientCounter--;
+            Clients.All.recalculateOnlineUsers(clientCounter);
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
---
+++ ChatApplication/ChatApplication/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Signar2.Startup))]
+namespace Signar2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}
